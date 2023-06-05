770e61f8f2d77a6e43923a370e430b6cc20d541d
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-05 15:13:22 -0600

    added flex-direction row with plants, added media query

---
+++ src/components/FlexRow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FlexRow = () => {
+    return (
+        <div>
+            <h2 class="title">flex-direction: row</h2>
+            <div id="flexRow">
+                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1685991886/portfolio%20site%20images/Project%20Images/Plant1_xk5dlg.png" alt="Plant1" />
+                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1685991891/portfolio%20site%20images/Project%20Images/Plant3_fao7sk.png" alt="Plant2" />
+                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1685991888/portfolio%20site%20images/Project%20Images/Plant2_f5r7bw.png" alt="Plant3" />
+            </div>
+        </div>
+    )
+}
+
+export default FlexRow;
