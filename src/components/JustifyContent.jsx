daa13d5eaae5a6679e0fef9d57b796987615d3ba
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-06 08:32:05 -0600

    added justify-content:flex-end

--- src/components/JustifyContent.jsx
+++ src/components/JustifyContent.jsx
@@ -3,14 +3,24 @@ import React from "react";
 const JustifyContent = () => {
     return (
         <div>
-            <h2 class="title">justify-content: flex-start</h2>
-            <div id="jcFlexStart">
-                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
-                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
-                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+            <div>
+                <h2 class="title">justify-content: flex-start</h2>
+                <div id="jcFlexStart">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
             </div>
+            <div>
+                <h2 class="title">justify-content: flex-end</h2>
+                <div id="jcFlexEnd">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
+            </div>
+
         </div>
-        
     )
 }
 
