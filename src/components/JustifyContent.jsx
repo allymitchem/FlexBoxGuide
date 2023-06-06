4ac2a0f3b43cf9d4229783b13c5719f411ca07ec
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-06 08:34:04 -0600

    added justify-content:center

--- src/components/JustifyContent.jsx
+++ src/components/JustifyContent.jsx
@@ -19,6 +19,14 @@ const JustifyContent = () => {
                     <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
                 </div>
             </div>
+            <div>
+                <h2 class="title">justify-content: center</h2>
+                <div id="jcCenter">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
+            </div>
 
         </div>
     )
