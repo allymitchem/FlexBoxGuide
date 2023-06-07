0bb98a00c378f5cdaea37e7d7f37447046770997
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-07 09:12:09 -0600

    added align-content: center

--- src/components/AlignContent.jsx
+++ src/components/AlignContent.jsx
@@ -20,7 +20,16 @@ const AlignContent = () => {
                     <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
                 </div>
             </div> 
-            </div>
+
+            <div>
+                <h2 class="title">align-content: center</h2>
+                <div id="acCenter">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
+            </div> 
+        </div>
     )
 }
 
