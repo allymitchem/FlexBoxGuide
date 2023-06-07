9a174b234bf73caa61b754a78974f8768c3b529b
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-07 09:22:01 -0600

    added stretch(align-content)

--- src/components/AlignContent.jsx
+++ src/components/AlignContent.jsx
@@ -29,6 +29,15 @@ const AlignContent = () => {
                     <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
                 </div>
             </div> 
+
+            <div>
+                <h2 class="title">align-content: stretch</h2>
+                <div id="acStretch">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
+            </div> 
         </div>
     )
 }
