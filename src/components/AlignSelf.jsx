061d553a43eb977f88784eddce94e1a981629c8c
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-14 11:09:03 -0600

    troubleshooting align self stretch, added align-self :flex end, changed a few of the media sizes for others

--- src/components/AlignSelf.jsx
+++ src/components/AlignSelf.jsx
@@ -3,11 +3,26 @@ import React from "react";
 const AlignSelf = () => {
     return (
         <div>
-            <h2 class="title">align-self: stretch</h2>
-            <div id="alignSelfStretch">
-            <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
-                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
-                <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+            
+                <h2 class="title">align-self: stretch</h2>
+            <div class="alignSelfParent">
+                <div id="alignSelfStretch">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                
+                </div>
+            </div>
+
+                <h2 class="title">align-self: flex-end</h2>
+          <div class="alignSelfParent">
+              
+                <div id="alignSelfFlexEnd">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                </div>
+                
             </div>
         </div>
     )
