980d9ec850e7e39665b5070bf16c0e72615ec797
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-14 12:13:11 -0600

    added align-items center

--- src/components/AlignItems.jsx
+++ src/components/AlignItems.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AlignItems = () => {
     return (
-        <div>
+        <div class="alignItems">
             <div>
                 <h2 class="title">align-items: flex-start</h2>
                 <div id="aiFlexStart">
@@ -26,6 +26,19 @@ const AlignItems = () => {
                     <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
                     <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
                 </div>
+
+                <div>
+                <h2 class="title">align-items: center</h2>
+                <div id="aiCenter">
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686004143/portfolio%20site%20images/Project%20Images/plant_onqctz.png" alt="Plant2" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003255/portfolio%20site%20images/Project%20Images/cactus_hssvwq.png" alt="Plant3" />
+                    <img class="plants" src="https://res.cloudinary.com/fsa2/image/upload/v1686003257/portfolio%20site%20images/Project%20Images/aloe_xl16ay.png" alt="Plant1" />
+                </div>
+            </div>
             </div>
 
 
